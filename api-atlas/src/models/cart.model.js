83bc74d6e43284ac992474a637ad403be4e49bdb
clemensjoe11-rgb <clemensjoe11@gmail.com>
2025-10-10 14:23:10 +0200

    Update cart.model.js

--- api-atlas/src/models/cart.model.js
+++ api-atlas/src/models/cart.model.js
@@ -1,10 +1,21 @@
 import mongoose from "mongoose";
-const schema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", unique: true },
-  items: [{
-    product: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
-    qty: { type: Number, default: 1 }
-  }],
-  updatedAt: { type: Date, default: Date.now }
-}, { timestamps: true });
-export default mongoose.model("Cart", schema);
+const { ObjectId } = mongoose.Schema.Types;
+
+const itemSchema = new mongoose.Schema(
+  {
+    product: { type: ObjectId, ref: "Product", required: true },
+    qty: { type: Number, min: 1, default: 1 }
+  },
+  { _id: false }
+);
+
+const cartSchema = new mongoose.Schema(
+  {
+    user: { type: ObjectId, ref: "User", unique: true, index: true, required: true },
+    items: { type: [itemSchema], default: [] },
+    updatedAt: { type: Date, default: Date.now }
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Cart", cartSchema);
